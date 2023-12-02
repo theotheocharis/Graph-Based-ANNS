e6cc45fc892a370dce8c92b32f9da907cdec49fa
Dimitris Kikidis <jimkikidis1998@gmail.com>
2023-12-02 12:05:37 +0200

    GNNS Algotithm - gnns.hpp include bug

--- include/gnns.hpp
+++ include/gnns.hpp
@@ -18,13 +18,20 @@ class GNNS {
     private:
         int E, R, N;
         int imageNumber;
+        std::vector<Image *> *data;
         // Graph representation as adjacency list
         std::vector<std::vector<Image *> *> *graph;
+
+        std::ofstream output;
     public:
-        GNNS(int, int, int, int);
+        GNNS(int, int, int, std::vector<Image *> *,const std::string&);
         ~GNNS();
-        std::vector<Image*> search(Image* query);
+        //std::vector<Image*> search(Image* query);
+        void search(Image* query);
         void constructGraph(std::vector<Image *> *,int);
+        std::vector<double> getTrueNeighbors(Image *image);
+
+        void outputResults(std::vector<std::pair<Image *, double>>, std::vector<double>, Image *query);
 };
 
 #endif
